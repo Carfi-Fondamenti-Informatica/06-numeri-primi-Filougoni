e8a9f34a9bd45aeaa950f115f85039e896460c42
Filougoni <116490094+Filougoni@users.noreply.github.com>
2022-12-02 16:58:15 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,17 @@
 #include <iostream>
 
 int main(){
+    int n, i;
+        cin>>n;
+        bool result;
+        i=n-1;
+        result =numeroprimo(n,i);
+        if (result ==1)
+        {
+            cout<<"numero primo";
+        }
+        else {
+            cout<<"numero non primo";
+        }
   return 0;
 }

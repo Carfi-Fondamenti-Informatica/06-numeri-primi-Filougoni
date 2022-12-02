0f5b6675ac311d93e71a6c0e384905e423d16f10
Filougoni <116490094+Filougoni@users.noreply.github.com>
2022-12-02 16:56:55 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,14 @@
+#include "lib.h"
+using namespace std;
+bool numeroprimo(int n, int i) {
+    int s = 0;
+    if (i < 2)
+        return true;
+        s = (n % i);
 
+    if (s == 0)
+        return false;
+        else
+        s = numeroprimo(n, i - 1);
+
+}
